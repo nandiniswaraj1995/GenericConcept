a71ae68d2ec842a2113f2e01e4dbed536941c7b8
unknown <nandiniswaraj13@gmail.com>
2021-03-22 16:13:14 +0530

    [Nandini swaraj]Added Find Maximut in 3 Generic value

--- GenericConcept/GenericConcept/GenericClass.cs
+++ GenericConcept/GenericConcept/GenericClass.cs
@@ -6,27 +6,9 @@ namespace GenericConcept
 {
     public class GenericClass<T> where T : IComparable
     {
-        public static int maximum(int x, int y, int z)
+        public static T maximum(T x, T y, T z)
         {
-            int max = x;
-            if (y.CompareTo(max) > 0)
-                max = y;
-            if (z.CompareTo(max) > 0)
-                max = z;
-            return max;
-        }
-        public static float maximum(float x, float y, float z)
-        {
-            float max = x;
-            if (y.CompareTo(max) > 0)
-                max = y;
-            if (z.CompareTo(max) > 0)
-                max = z;
-            return max;
-        }
-        public static string maximum(string x, string y, string z)
-        {
-            string max = x;
+            T max = x;
             if (y.CompareTo(max) > 0)
                 max = y;
             if (z.CompareTo(max) > 0)
