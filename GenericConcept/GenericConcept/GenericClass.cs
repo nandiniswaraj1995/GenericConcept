56512360329768dd47ed2a039bf6cef07df95352
unknown <nandiniswaraj13@gmail.com>
2021-03-22 16:01:42 +0530

    [Nandini swaraj]Added Find Maximut in 3 integer value

---
+++ GenericConcept/GenericConcept/GenericClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericConcept
+{
+    public class GenericClass<T> where T : IComparable
+    {
+        public static int maximum(int x, int y, int z)
+        {
+            int max = x;
+            if (y.CompareTo(max) > 0)
+                max = y;
+            if (z.CompareTo(max) > 0)
+                max = z;
+            return max;
+        }
+
+    }
+}
---
+++ GenericConcept/GenericUnitTestProject/UnitTest1.cs
@@ -0,0 +1,29 @@
+using GenericConcept;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            [TestMethod]
+            public void Given_MaxIntegerNumber_1st_Position_ShouldReturnSame()
+            {
+                int max = GenericClass<int>.maximum(8, 5, 6);
+                Assert.AreEqual(8, max);
+            }
+            [TestMethod]
+            public void Given_MaxIntegerNumber_2nd_Position_ShouldReturnSame()
+            {
+                int max = GenericClass<int>.maximum(5, 8, 6);
+                Assert.AreEqual(8, max);
+            }
+
+            [TestMethod]
+            public void Given_MaxIntegerNumber_3rd_Position_ShouldReturnSame()
+            {
+                int max = GenericClass<int>.maximum(5, 6, 8);
+                Assert.AreEqual(8, max);
+            }
+        }
+    }
